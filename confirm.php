090fcf9023246776ae0ca7eff7beb43e10b6251f
Damandeep Singh <singh_dd93@yahoo.in>
2014-03-01 21:45:56 +0530

    Fix the Signup Process
    Finally got it working .. :)

--- confirm.php
+++ confirm.php
@@ -7,48 +7,89 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Confirmation Sent</title>
-        <?php include 'includes/db.php' ;
-        
-        function random_numbers($digits) 
-            {
+        <title>Confirmation</title>
+        <?php
+        include 'includes/db.php';
+
+//        if($conn)
+//        {
+//            echo "Connection Sucess";
+//        }
+
+        function random_numbers($digits) {
             $min = pow(10, $digits - 1);
             $max = pow(10, $digits) - 1;
             return mt_rand($min, $max);
-            }
-    ?>;
+        }
+        ?>
 
     </head>
-    <body>
-        <p>We have sent you a confimation email. Kindly click on the link 
-        provided in the mail to activate your account.<br>
-        Thankyou! </p>
+    <body>  
         <?php
         // put your code here
-        if(isset($_POST['submit']))
-    {
-        $em=$_POST['email'];
-        $psd=$_POST['pass'];
-        $query="Select * from users_reg WHERE email= ? ";
-        
-        $q=$conn->prepare($query);
-        if($q === FALSE)
-        {
-            trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+        if (isset($_POST['submit'])) {
+            $em = $_POST['email'];
+            $psd = $_POST['pass'];
+
+            $getquery = "Select * from users_reg WHERE `email`= ? ;";
+
+            $q = $conn->prepare($getquery);
+            if ($q === FALSE) {
+                trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+            }
+            $q->bind_param('s', $em);
+            $q->execute();
+            $q->store_result();
+            if ($q->num_rows > 0) {
+                echo 'YOu have Entered the Email : ' . "$em";
+                echo "<p>" . " You are already registered. Login to enter." . "</p>";
+                $q->close();
+                $conn->close();
+            } else {
+                $q->close();
+
+                $insquery = "INSERT INTO users_reg(`email`,`password`,`profile_id`,`confirm_code`) VALUES(?,?,?,?)";
+                $qs = $conn->prepare($insquery);
+
+                if ($q1 === FALSE) {
+                    trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+                }
+
+                $qs->bind_param('ssis', $ems, $pass, $pid, $cc);
+                $ems = $em;
+                $pass = sha1($psd) . md5($psd);
+                $pid = random_numbers(10);
+                $cc = md5($em);
+                $qs->execute();
+                $rows = $qs->affected_rows;
+                $qs->close();
+                $conn->close();
+                if ($rows == 1) {
+                    $subject = 'Confirm your Email Address';
+                    $message = "Dear User"
+                            . " \n Please Click on the link below to confirm your "
+                            . " E-mail Address \n "
+                            . "http://learning.singhdd.com/econfirm.php?email=$em&code=$cc"
+                            . "\n If you are unable to open link directly please copy and "
+                            . "paste the link in your browser. \n Thanks";
+
+                    $header = "From: singh.damandeep@gmail.com \r\n";
+                    $param = "-f singh.damandeep@gmail.com";
+
+                    $send_mail = mail($em, $subject, $message, $headers, $param);
+
+                    if ($send_mail) {
+                        echo "<p>" . 'We have sent you a confimation email. Kindly click on the link'
+                        . ' provided in the mail to activate your account.' . "</p>";
+                        echo "<p>" . 'Thankyou!' . "</p>";
+                    } else {
+                        echo "Problem Sending Mail";
+                    }
+                } else {
+                    echo "Problem in Registration";
+                }
+            }
         }
-        $q->bind_param('s',$em);
-        $q->execute();
-        
-        if($q->num_rows>0)
-        {
-            echo "<p>"." You are already registered. Login to enter."."</p>";
-        }
- else {
-     $pid=random_numbers(10);
-     $cc= md5($em);
-     $query="INSERT INTO users_reg VALUES(NULL,$em,$psd,$pid,$cc)"; 
- }
-    }
         ?>
     </body>
 </html>
--- profile.php
+++ profile.php
@@ -8,47 +8,30 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Profile</title>
-        <?php include 'includes/db.php' ?>;
+        <?php include 'includes/db.php' ?>
     </head>
     <body>
         <?php
-    if(isset($_POST['sub']))
-    {
-        $un=$_POST['user_name'];
-        $ps=$_POST['pass'];
-        $query="Select profile_id from users_info WHERE user_name= ? ";
-        
-                                                //        $sql='SELECT lastname, email FROM customers WHERE id > ? AND firstname = ?';
-                                                //$id_greater_than = 5;
-                                                //$firstname = 'John';
-                                                // 
-                                                ///* Prepare statement */
-                                                //$stmt = $conn->prepare($sql);
-                                                //if($stmt === false) {
-                                                //  trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-                                                //}
-                                                // 
-                                                ///* Bind parameters. TYpes: s = string, i = integer, d = double,  b = blob */
-                                                //$stmt->bind_param('is',$id_greater_than,$firstname);
-                                                // 
-                                                ///* Execute statement */
-                                                //$stmt->execute();
-        
-        $a=$conn->prepare($query);
-        if($a === FALSE)
-        {
-            trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-        }
-        $a->bind_param('s',$un);
-        $a->execute();
-        
-        echo "$un"."Pass: "."$ps";
-        ?>
-        <script type="text/javascript">
-            alert("Logged In");
-        </script>
-        <?php
-    }
-    ?>
+        if (isset($_POST['sub'])) {
+            $un = $_POST['user_name'];
+            $ps = $_POST['pass'];
+            $query = "Select profile_id from users_info WHERE user_name= ? ";
+
+
+            $a = $conn->prepare($query);
+            if ($a === FALSE) {
+                trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+            }
+            $a->bind_param('s', $un);
+            $a->execute();
+
+            echo "$un" . "Pass: " . "$ps";
+            ?>
+            <script type="text/javascript">
+                alert("Logged In");
+            </script>
+    <?php
+}
+?>
     </body>
 </html>
