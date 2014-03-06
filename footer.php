ae533aaf6d8d077eedfa8242d215f691121a5e6a
Damandeep Singh <singh_dd93@yahoo.in>
2014-03-07 02:22:39 +0530

    Make the Template more personal
    Aggadhifying the Template :)
    Changed the colours
    Separated the header and footer
    SetUp the signup page( Temp. looks)

---
+++ footer.php
@@ -0,0 +1,101 @@
+<!-- start: Footer Menu -->
+        <div id="footer-menu" class="hidden-tablet hidden-phone">
+
+            <!-- start: Container -->
+            <div class="container">
+
+                <!-- start: Row -->
+                <div class="row">
+
+                    <!-- start: Footer Menu Logo -->
+                    <div class="span2">
+                        <div id="footer-menu-logo">
+                            <a href="#"><img src="img/logo-footer-menu.png" alt="logo" /></a>
+                        </div>
+                    </div>
+                    <!-- end: Footer Menu Logo -->
+
+                    <!-- start: Footer Menu Links-->
+                    <div class="span9">
+
+                        <div id="footer-menu-links">
+
+                            <ul id="footer-nav">
+
+                                <li><a href="index.php">Home</a></li>
+
+                                <li><a href="about.html">About</a></li>
+
+                                <li><a href="services.html">Services</a></li>
+
+                                <li><a href="contact.html">Contact</a></li>
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+                    <!-- end: Footer Menu Links-->
+
+                    <!-- start: Footer Menu Back To Top -->
+                    <div class="span1">
+
+                        <div id="footer-menu-back-to-top">
+                            <a href="#"></a>
+                        </div>
+
+                    </div>
+                    <!-- end: Footer Menu Back To Top -->
+
+                </div>
+                <!-- end: Row -->
+
+            </div>
+            <!-- end: Container  -->	
+
+        </div>	
+        <!-- end: Footer Menu -->
+
+        <!-- start: Copyright -->
+        <div id="copyright">
+
+            <!-- start: Container -->
+            <div class="container">
+
+                <p>
+                    &copy; 2014, AGGADH.
+                </p>
+
+            </div>
+            <!-- end: Container  -->
+
+        </div>	
+        <!-- end: Copyright -->
+
+        <!-- start: Java Script -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="js/jquery-1.8.2.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/flexslider.js"></script>
+        <script src="js/carousel.js"></script>
+        <script src="js/jquery.cslider.js"></script>
+        <script src="js/slider.js"></script>
+        <script defer="defer" src="js/custom.js"></script>
+        
+<!--        <script type="text/javascript">
+        $(document).ready(function () {
+    $('.nav li a').click(function(e) {
+
+        $('.nav li').removeClass('active');
+
+        var $parent = $(this).parent();
+        if (!$parent.hasClass('active')) {
+            $parent.addClass('active');
+        }
+        e.preventDefault();
+    });
+});</script>-->
+        <!-- end: Java Script -->
+
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!-- start: Meta -->
+        <meta charset="utf-8">
+        <title>AGGADH</title> 
+        <meta name="description" content="AGGADH"/>
+        <meta name="keywords" content="AGGADH,e-learning,quiz,application" />
+        <!-- end: Meta -->
+
+        <!-- start: Mobile Specific -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <!-- end: Mobile Specific -->
+
+        <!-- start: Facebook Open Graph -->
+        <meta property="og:title" content=""/>
+        <meta property="og:description" content=""/>
+        <meta property="og:type" content=""/>
+        <meta property="og:url" content=""/>
+        <meta property="og:image" content=""/>
+        <!-- end: Facebook Open Graph -->
+
+        <? include './includes/db.php';
+        
+        session_start();
+        
+        if(! isset($_SESSION['user']))
+        {
+            ?>
+                <script type="text/javascript">
+                   // alert("Not Logged In");
+                </script>   
+         
+            <?
+        }
+        
+        
+        function echoActiveClassIfRequestMatches($requestUri) {
+                    $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
+
+                    if ($current_file_name == $requestUri)
+                        echo 'active';
+                }
+                ?>
+        
+        
+        
+        <!-- start: CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+        <!-- end: CSS -->
+
+        
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+       
+    </head>
+    <body>
+
+        <!--start: Header -->
+        <header>
+
+            <!--start: Container -->
+            <div class="container">
+
+                <!--start: Row -->
+                <div class="row">
+
+                    <!--start: Logo -->
+                    <div class="logo span3">
+
+                        <a class="brand" href="#"><img src="img/logo.png" alt="Logo"></a>
+
+                    </div>
+                    <!--end: Logo -->
+
+                    <!--start: Navigation -->
+                    <div class="span9">
+
+                        <div class="navbar navbar-inverse">
+                            <div class="navbar-inner">
+                                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </a>
+                                <div class="nav-collapse collapse">
+                                    <ul class="nav">
+                                        <li class="<?=echoActiveClassIfRequestMatches("index")?>"><a href="index.php">Home</a></li>
+                                        <li class="<?=echoActiveClassIfRequestMatches("about")?>"><a href="">About</a></li>
+                                        <li class="<?=echoActiveClassIfRequestMatches("services")?>"><a href="">Services</a></li>
+                                        <li class="<?=echoActiveClassIfRequestMatches("contact")?>"><a href="">Contact</a></li>
+                                        <li class="<?=echoActiveClassIfRequestMatches("signup")?>"><a href="signup.php">Sign Up</a></li>
+                                        <li class="dropdown ">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Log In <b class="caret"></b></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="#">Action</a></li>
+                                                <li><a href="#">Another action</a></li>
+                                                <li><a href="#">Something else here</a></li>
+                                                <li class="divider"></li>
+                                                <li class="nav-header">Nav header</li>
+                                                <li><a href="#">Separated link</a></li>
+                                                <li><a href="#">One more separated link</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>	
+                    <!--end: Navigation -->
+
+                </div>
+                <!--end: Row -->
+
+            </div>
+            <!--end: Container-->			
+
+        </header>
+        <!--end: Header-->
