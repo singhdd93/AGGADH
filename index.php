d5e099e65cc34f1d1e433f3ffd7d99adc7495d7e
Gurjot Singh <bhattigurjot@gmail.com>
2014-03-06 20:33:58 +0530

    Add new theme

---
+++ index.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!-- start: Meta -->
+        <meta charset="utf-8">
+        <title>AGGADH</title> 
+        <meta name="description" content="AGGADH"/>
+        <meta name="keywords" content="AGGADH,e-learning,quiz,application" />
+        <!-- end: Meta -->
+
+        <!-- start: Mobile Specific -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <!-- end: Mobile Specific -->
+
+        <!-- start: Facebook Open Graph -->
+        <meta property="og:title" content=""/>
+        <meta property="og:description" content=""/>
+        <meta property="og:type" content=""/>
+        <meta property="og:url" content=""/>
+        <meta property="og:image" content=""/>
+        <!-- end: Facebook Open Graph -->
+
+        <?php include 'header.php';?>
+        
+        <!-- start: CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+        <!-- end: CSS -->
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+
+        <!--start: Header -->
+        <header>
+
+            <!--start: Container -->
+            <div class="container">
+
+                <!--start: Row -->
+                <div class="row">
+
+                    <!--start: Logo -->
+                    <div class="logo span3">
+
+                        <a class="brand" href="#"><img src="img/logo.png" alt="Logo"></a>
+
+                    </div>
+                    <!--end: Logo -->
+
+                    <!--start: Navigation -->
+                    <div class="span9">
+
+                        <div class="navbar navbar-inverse">
+                            <div class="navbar-inner">
+                                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </a>
+                                <div class="nav-collapse collapse">
+                                    <ul class="nav">
+                                        <li class="active"><a href="#">Home</a></li>
+                                        <li><a href="#">About</a></li>
+                                        <li><a href="#">Services</a></li>
+                                        <li><a href="#">Contact</a></li>
+                                        <li><a href="signup.php">Sign Up</a></li>
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Log In <b class="caret"></b></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="#">Action</a></li>
+                                                <li><a href="#">Another action</a></li>
+                                                <li><a href="#">Something else here</a></li>
+                                                <li class="divider"></li>
+                                                <li class="nav-header">Nav header</li>
+                                                <li><a href="#">Separated link</a></li>
+                                                <li><a href="#">One more separated link</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>	
+                    <!--end: Navigation -->
+
+                </div>
+                <!--end: Row -->
+
+            </div>
+            <!--end: Container-->			
+
+        </header>
+        <!--end: Header-->
+
+        <!-- start: Slider -->
+        <div class="slider-wrapper">
+
+            <div id="da-slider" class="da-slider">
+                <div class="da-slide">
+                    <h2>Twitter Bootstrap</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <a href="#" class="da-link">Read more</a>
+                    <div class="da-img"><img src="img/parallax-slider/twitter.png" alt="image01" /></div>
+                </div>
+                <div class="da-slide">
+                    <h2>Responsive Design</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <a href="#" class="da-link">Read more</a>
+                    <div class="da-img"><img src="img/parallax-slider/responsive.png" alt="image02" /></div>
+                </div>
+                <div class="da-slide">
+                    <h2>HTML5</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <a href="#" class="da-link">Read more</a>
+                    <div class="da-img"><img src="img/parallax-slider/html5.png" alt="image03" /></div>
+                </div>
+                <div class="da-slide">
+                    <h2>CSS3</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <a href="#" class="da-link">Read more</a>
+                    <div class="da-img"><img src="img/parallax-slider/css3.png" alt="image04" /></div>
+                </div>
+                <nav class="da-arrows">
+                    <span class="da-arrows-prev"></span>
+                    <span class="da-arrows-next"></span>
+                </nav>
+            </div>
+
+        </div>
+        <!-- end: Slider -->
+
+        <!--start: Wrapper-->
+        <div id="wrapper">
+
+            <!--start: Container -->
+            <div class="container">
+
+                <!-- start: Hero Unit - Main hero unit for a primary marketing message or call to action -->
+                <div class="hero-unit">
+                    <p>AGGADH is an E-Learning and Quiz application. <br>
+                        Designed and built to provide extensive learning for various subjects.
+                    </p>
+                    <p><a class="btn btn-success btn-large">Learn more &raquo;</a></p>
+                </div>
+                <!-- end: Hero Unit -->
+
+                <!-- start: Row -->
+                <div class="row">
+
+                    <div class="span4">
+                        <div class="icons-box">
+                            <i class="ico-ok circle big"></i>
+                            <div class="title"><h3>Easy to use</h3></div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                            </p>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+
+                    <div class="span4">
+                        <div class="icons-box">
+                            <i class="ico-ipad circle big"></i>
+                            <div class="title"><h3>Responsive</h3></div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                            </p>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+
+                    <div class="span4">
+                        <div class="icons-box">
+                            <i class="ico-heart circle big"></i>
+                            <div class="title"><h3>User friendly</h3></div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                            </p>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- end: Row -->
+
+                <hr>
+
+                <!-- start Clients List -->	
+                <div class="clients-carousel">
+
+                    <ul class="slides clients">
+                        <li><img src="img/logos/1.png" alt=""/></li>
+                        <li><img src="img/logos/2.png" alt=""/></li>	
+                        <li><img src="img/logos/3.png" alt=""/></li>
+                        <li><img src="img/logos/4.png" alt=""/></li>
+                        <li><img src="img/logos/5.png" alt=""/></li>
+                        <li><img src="img/logos/6.png" alt=""/></li>
+                        <li><img src="img/logos/7.png" alt=""/></li>
+                        <li><img src="img/logos/8.png" alt=""/></li>
+                        <li><img src="img/logos/9.png" alt=""/></li>
+                        <li><img src="img/logos/10.png" alt=""/></li>		
+                    </ul>
+
+                </div>
+                <!-- end Clients List -->
+
+                <hr>
+
+                <!-- start: Row -->
+                <div class="row">
+
+                    <!-- start: Icon Boxes -->
+                    <div class="icons-box-vert-container">
+
+                        <!-- start: Icon Box Start -->
+                        <div class="span6">
+                            <div class="icons-box-vert">
+                                <i class="ico-ok ico-color circle-color big"></i>
+                                <div class="icons-box-vert-info">
+                                    <h3>Easy to use</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        <!-- end: Icon Box-->
+
+                        <!-- start: Icon Box Start -->
+                        <div class="span6">
+                            <div class="icons-box-vert">
+                                <i class="ico-cup  ico-white circle-color-full big-color"></i>
+                                <div class="icons-box-vert-info">
+                                    <h3>Best choice</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        <!-- end: Icon Box -->
+
+                        <!-- start: Icon Box Start -->
+                        <div class="span6">
+                            <div class="icons-box-vert">
+                                <i class="ico-ipad ico-color circle-color big"></i>
+                                <div class="icons-box-vert-info">
+                                    <h3>Fully Responsive</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        <!-- end: Icon Box -->
+
+                        <!-- start: Icon Box Start -->
+                        <div class="span6">
+                            <div class="icons-box-vert">
+                                <i class="ico-thumbs-up  ico-white circle-color-full big-color"></i>
+                                <div class="icons-box-vert-info">
+                                    <h3>Social Network</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        <!-- end: Icon Box -->
+
+                    </div>
+                    <!-- end: Icon Boxes -->
+                    <div class="clear"></div>
+                </div>
+                <!-- end: Row -->
+
+                <hr>
+
+            </div>
+            <!--end: Container-->
+
+        </div>
+        <!-- end: Wrapper  -->			
+
+        <!-- start: Footer Menu -->
+        <div id="footer-menu" class="hidden-tablet hidden-phone">
+
+            <!-- start: Container -->
+            <div class="container">
+
+                <!-- start: Row -->
+                <div class="row">
+
+                    <!-- start: Footer Menu Logo -->
+                    <div class="span2">
+                        <div id="footer-menu-logo">
+                            <a href="#"><img src="img/logo-footer-menu.png" alt="logo" /></a>
+                        </div>
+                    </div>
+                    <!-- end: Footer Menu Logo -->
+
+                    <!-- start: Footer Menu Links-->
+                    <div class="span9">
+
+                        <div id="footer-menu-links">
+
+                            <ul id="footer-nav">
+
+                                <li><a href="index.html">Start</a></li>
+
+                                <li><a href="about.html">About</a></li>
+
+                                <li><a href="services.html">Services</a></li>
+
+                                <li><a href="pricing.html">Pricing</a></li>
+
+                                <li><a href="contact.html">Contact</a></li>
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+                    <!-- end: Footer Menu Links-->
+
+                    <!-- start: Footer Menu Back To Top -->
+                    <div class="span1">
+
+                        <div id="footer-menu-back-to-top">
+                            <a href="#"></a>
+                        </div>
+
+                    </div>
+                    <!-- end: Footer Menu Back To Top -->
+
+                </div>
+                <!-- end: Row -->
+
+            </div>
+            <!-- end: Container  -->	
+
+        </div>	
+        <!-- end: Footer Menu -->
+
+        <!-- start: Copyright -->
+        <div id="copyright">
+
+            <!-- start: Container -->
+            <div class="container">
+
+                <p>
+                    &copy; 2014, AGGADH. designed by Us
+                </p>
+
+            </div>
+            <!-- end: Container  -->
+
+        </div>	
+        <!-- end: Copyright -->
+
+        <!-- start: Java Script -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="js/jquery-1.8.2.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/flexslider.js"></script>
+        <script src="js/carousel.js"></script>
+        <script src="js/jquery.cslider.js"></script>
+        <script src="js/slider.js"></script>
+        <script defer="defer" src="js/custom.js"></script>
+        <!-- end: Java Script -->
+
+    </body>
+</html>
