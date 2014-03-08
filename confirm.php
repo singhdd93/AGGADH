9424e54651ef3e6536e7373433b414a9944bae20
Damandeep Singh <singh_dd93@yahoo.in>
2014-03-08 08:01:47 +0530

    Theme confirm page

--- confirm.php
+++ confirm.php
@@ -1,92 +1,135 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Confirmation</title>
-        <?php
-        include 'includes/db.php';
+<?php
+include './header.php';
 
-//        if($conn)
-//        {
-//            echo "Connection Sucess";
-//        }
+function getProfileid() {
+    $pi = uniqid();
+    $pis = hexdec($pi);
+    $len = strlen($pis);
+    $st = $len - 8;
+    $ret_id = substr($pis, $st);
+    return $ret_id;
+}
+?>
 
-        ?>
+<div id="page-title">
 
-    </head>
-    <body>  
-        <?php
-        // put your code here
-        if (isset($_POST['submit'])) {
-            $em = $_POST['email'];
-            $psd = $_POST['pass'];
+    <div id="page-title-inner">
 
-            $getquery = "Select * from users_reg WHERE `email`= ? ;";
+        <!-- start: Container -->
+        <div class="container">
 
-            $q = $conn->prepare($getquery);
-            if ($q === FALSE) {
-                trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-            }
-            $q->bind_param('s', $em);
-            $q->execute();
-            $q->store_result();
-            if ($q->num_rows > 0) {
-                echo 'YOu have Entered the Email : ' . "$em";
-                echo "<p>" . " You are already registered. Login to enter." . "</p>";
-                $q->close();
-                $conn->close();
-            } else {
-                $q->close();
+            <h2><i class="ico-stats ico-white"></i>Sign Up</h2>
 
-                $pi = uniqid("",true);
-                
-                $insquery = "INSERT INTO users_reg(`email`,`password`,`profile_id`,`confirm_code`) VALUES(?,?,?,?)";
-                $qs = $conn->prepare($insquery);
+        </div>
+        <!-- end: Container  -->
 
-                if ($qs === FALSE) {
-                    trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-                }
+    </div>	
 
-                $qs->bind_param('ssis', $ems, $pass, $pid, $cc);
-                $ems = $em;
-                $pass = sha1($psd) . md5($psd);
-                $pid = substr($pi, 0, 10);
-                $cc = md5($em);
-                $qs->execute();
-                $rows = $qs->affected_rows;
-                $qs->close();
-                $conn->close();
-                if ($rows == 1) {
-                    $subject = 'Confirm your Email Address';
-                    $message = "Dear User"
-                            . " \n Please Click on the link below to confirm your "
-                            . " E-mail Address \n "
-                            . "http://learning.singhdd.com/econfirm.php?email=$em&code=$cc"
-                            . "\n If you are unable to open link directly please copy and "
-                            . "paste the link in your browser. \n Thanks";
+</div>
+<!-- end: Page Title -->
 
-                    $headers = "From: singh.damandeep@gmail.com \r\n";
-                    $param = "-f singh.damandeep@gmail.com";
+<!--start: Wrapper-->
+<div id="wrapper">
 
-                    $send_mail = mail($em, $subject, $message, $headers, $param);
+    <!--start: Container -->
+    <div class="container">
 
-                    if ($send_mail) {
-                        echo "<p>" . 'We have sent you a confimation email. Kindly click on the link'
-                        . ' provided in the mail to activate your account.' . "</p>";
-                        echo "<p>" . 'Thankyou!' . "</p>";
-                    } else {
-                        echo "Problem Sending Mail";
+        <!--start: Row -->
+        <div class="row">
+
+            <div class="span12">
+
+
+                <div class="message">
+
+                    <?php
+                    // put your code here
+                    if (isset($_POST['submit'])) {
+                        $em = $_POST['email'];
+                        $psd = $_POST['pass'];
+
+                        $getquery = "Select * from users_reg WHERE `email`= ? ;";
+
+                        $q = $conn->prepare($getquery);
+                        if ($q === FALSE) {
+                            trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+                        }
+                        $q->bind_param('s', $em);
+                        $q->execute();
+                        $q->store_result();
+                        if ($q->num_rows > 0) {
+                            echo "<p>" . 'YOu have Entered the Email : ' . "$em". "</p>";
+                            echo "<p>" . " You are already registered. Login to enter." . "</p>";
+                            $q->close();
+                            $conn->close();
+                        } else {
+                            $q->close();
+
+                            $insquery = "INSERT INTO users_reg(`email`,`password`,`profile_id`,`confirm_code`) VALUES(?,?,?,?)";
+                            $qs = $conn->prepare($insquery);
+
+                            if ($qs === FALSE) {
+                                trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+                            }
+
+                            $qs->bind_param('ssis', $ems, $pass, $pid, $cc);
+                            $ems = $em;
+                            $pass = sha1($psd) . md5($psd);
+                            $pid = getProfileid();
+                            $cc = md5($em);
+                            $qs->execute();
+                            $rows = $qs->affected_rows;
+                            $qs->close();
+                            $conn->close();
+                            if ($rows == 1) {
+                                $subject = 'Confirm your Email Address';
+                                $message = "Dear User"
+                                        . " \n Please Click on the link below to confirm your "
+                                        . " E-mail Address \n "
+                                        . "http://learning.singhdd.com/econfirm.php?email=$em&code=$cc"
+                                        . "\n If you are unable to open link directly please copy and "
+                                        . "paste the link in your browser. \n Thanks";
+
+                                $headers = "From: singh.damandeep@gmail.com \r\n";
+                                $param = "-f singh.damandeep@gmail.com";
+
+                                $send_mail = mail($em, $subject, $message, $headers, $param);
+
+                                if ($send_mail) {
+                                    echo "<p>" . 'We have sent you a confimation email. Kindly click on the link'
+                                    . ' provided in the mail to activate your account.' . "</p>";
+                                    echo "<p>" . 'Thankyou!' . "</p>";
+                                } else {
+                                    echo "Problem Sending Mail";
+                                }
+                            } else {
+                                echo "Problem in Registration";
+                            }
+                        }
                     }
-                } else {
-                    echo "Problem in Registration";
-                }
-            }
-        }
-        ?>
-    </body>
-</html>
+                    ?>
+
+                </div>
+
+
+
+
+            </div>	
+
+
+
+        </div>
+        <!--end: Row-->
+
+    </div>
+    <!--end: Container-->
+
+
+
+</div>
+<!-- end: Wrapper  -->	
+
+
+
+<? include './footer.php'; ?>
+
