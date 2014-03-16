95547158ce59b43e53cb8de582fc03616045da04
Damandeep Singh <singh_dd93@yahoo.in>
2014-03-16 17:39:26 +0530

    Update sql file

--- sql/aggadh.sql
+++ sql/aggadh.sql
@@ -1,31 +1,52 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.8.1deb1
+-- version 4.1.8
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 03, 2014 at 02:01 PM
--- Server version: 5.5.34-0ubuntu0.13.04.1
--- PHP Version: 5.4.9-4ubuntu2.4
+-- Host: localhost:3306
+-- Generation Time: Mar 16, 2014 at 05:37 PM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.25
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `aggadh`
 --
+CREATE DATABASE IF NOT EXISTS `aggadh` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `aggadh`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `a_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `a_username` varchar(255) NOT NULL,
+  `a_password` text NOT NULL,
+  `a_name` varchar(255) NOT NULL,
+  `a_dob` date NOT NULL,
+  `a_sex` varchar(1) NOT NULL,
+  `a_email` text NOT NULL,
+  `a_lastlogin` datetime NOT NULL,
+  `a_since` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`a_id`),
+  UNIQUE KEY `a_username` (`a_username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`a_id`, `a_username`, `a_password`, `a_name`, `a_dob`, `a_sex`, `a_email`, `a_lastlogin`, `a_since`) VALUES
+(1, 'singhdd', '58ba7dfcfc0718df66e940a9d483b7bc', 'Damandeep Singh', '1993-08-03', 'm', 'singh.damandeep@gmail.com', '2013-11-30 00:00:00', '2014-03-16 08:04:46');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `branches`
---
--- Creation: Mar 03, 2014 at 08:31 AM
 --
 
 CREATE TABLE IF NOT EXISTS `branches` (
@@ -41,8 +62,6 @@ CREATE TABLE IF NOT EXISTS `branches` (
 --
 -- Table structure for table `branch_sub_map`
 --
--- Creation: Mar 03, 2014 at 08:31 AM
---
 
 CREATE TABLE IF NOT EXISTS `branch_sub_map` (
   `branch_id` int(11) NOT NULL,
@@ -53,8 +72,6 @@ CREATE TABLE IF NOT EXISTS `branch_sub_map` (
 
 --
 -- Table structure for table `level`
---
--- Creation: Mar 03, 2014 at 08:31 AM
 --
 
 CREATE TABLE IF NOT EXISTS `level` (
@@ -67,8 +84,6 @@ CREATE TABLE IF NOT EXISTS `level` (
 
 --
 -- Table structure for table `quest_web_tech`
---
--- Creation: Mar 03, 2014 at 08:31 AM
 --
 
 CREATE TABLE IF NOT EXISTS `quest_web_tech` (
@@ -89,8 +104,6 @@ CREATE TABLE IF NOT EXISTS `quest_web_tech` (
 --
 -- Table structure for table `subjects`
 --
--- Creation: Mar 03, 2014 at 08:31 AM
---
 
 CREATE TABLE IF NOT EXISTS `subjects` (
   `sub_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -105,8 +118,6 @@ CREATE TABLE IF NOT EXISTS `subjects` (
 --
 -- Table structure for table `sub_topic_map`
 --
--- Creation: Mar 03, 2014 at 08:31 AM
---
 
 CREATE TABLE IF NOT EXISTS `sub_topic_map` (
   `sub_id` int(11) NOT NULL,
@@ -117,8 +128,6 @@ CREATE TABLE IF NOT EXISTS `sub_topic_map` (
 
 --
 -- Table structure for table `topics`
---
--- Creation: Mar 03, 2014 at 08:31 AM
 --
 
 CREATE TABLE IF NOT EXISTS `topics` (
@@ -134,45 +143,35 @@ CREATE TABLE IF NOT EXISTS `topics` (
 --
 -- Table structure for table `users_info`
 --
--- Creation: Mar 03, 2014 at 08:31 AM
---
 
 CREATE TABLE IF NOT EXISTS `users_info` (
   `name` text NOT NULL,
   `dob` date NOT NULL,
   `contact_no` char(13) NOT NULL,
   `sex` char(1) NOT NULL,
-  `profile_id` int(11) NOT NULL,
+  `profile_id` bigint(11) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   UNIQUE KEY `user_name` (`user_name`),
   KEY `profile_id` (`profile_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELATIONS FOR TABLE `users_info`:
---   `profile_id`
---       `users_reg` -> `profile_id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users_reg`
 --
--- Creation: Mar 03, 2014 at 08:31 AM
---
 
 CREATE TABLE IF NOT EXISTS `users_reg` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` text NOT NULL,
-  `profile_id` int(11) NOT NULL,
+  `profile_id` bigint(11) NOT NULL,
   `confirm_code` varchar(255) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '0',
+  `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`,`profile_id`),
-  KEY `profile_id` (`profile_id`),
-  KEY `profile_id_2` (`profile_id`)
+  UNIQUE KEY `profile_id` (`profile_id`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -183,8 +182,4 @@ CREATE TABLE IF NOT EXISTS `users_reg` (
 -- Constraints for table `users_info`
 --
 ALTER TABLE `users_info`
-  ADD CONSTRAINT `link_pid_of_reg_to_info` FOREIGN KEY (`profile_id`) REFERENCES `users_reg` (`profile_id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  ADD CONSTRAINT `usr_reg_pid_to_user_info_pid` FOREIGN KEY (`profile_id`) REFERENCES `users_reg` (`profile_id`);
