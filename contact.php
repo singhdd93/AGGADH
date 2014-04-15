57a941ccef4b665df74989797fa1ead8de0c07a8
Damandeep Singh <singh_dd93@yahoo.in>
2014-04-15 19:02:20 +0530

    Update Contacts Page

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 			<!-- start: Container -->
 			<div class="container">
 
-				<h2><i class="ico-circle-plus ico-white"></i>Sign Up</h2>
+				<h2><i class="ico-circle-plus ico-white"></i>Contact Us</h2>
 
 			</div>
 			<!-- end: Container  -->
@@ -31,18 +31,20 @@
 					
 					
 					<div id="signup">
-						<div class="title"><h3>Contact Us</h3></div>
+						<div class="title"><h3>Send Us a Message with your Query / Review</h3></div>
+                                                
+                                                
 					
                                                 <div class="signup-form">
-                                            <form action="confirm.php" method="POST">
+                                            <form action="sendconemail.php" method="POST">
                                                 <div class="input">
-                                                <input type="text" name="uname" value="" id="name" placeholder="Name" />
+                                                <input type="text" name="uname" required="required" value="" id="name" placeholder="Name" />
                                                 <div class="input">
-                                                <input type="email" name="email" value="" placeholder="Email" />
+                                                <input type="email" name="email" required="required" value="" placeholder="Email" />
                                                 </div>
                                                                                                
                                                 <div class="input">
-                                                <input type="textarea" name="message" value="" placeholder="Message" />
+                                                    <textarea  name="message" required="required"  placeholder="Message" ></textarea>
                                                 </div>
                                                 
                                               <!--  <div class="input">
@@ -50,7 +52,7 @@
                                                 <input type="text" name="captcha" value="" placeholder="Confirm Password" />
                                                 </div>-->
                                                 <div class="actions">
-                                                <input type="submit" name="submit" value="Sign Up" />
+                                                <input type="submit" name="submit" value="Submit" />
                                                 </div>
                                             </form>
                                                 </div>
