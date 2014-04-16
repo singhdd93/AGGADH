dc860574220b5571263d47edd71cf52fc4cdf6ca
Damandeep Singh <singh_dd93@yahoo.in>
2014-04-16 05:48:26 +0530

    Update About Page

--- about.php
+++ about.php
@@ -33,21 +33,61 @@
 					<div id="about">
 						<div class="title"><h3>About Us</h3></div>
 						<p>
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                                                    <span class="dropcap color">A</span>GGADH is an E-Learning and Quiz application. It is designed and built to provide extensive learning for various subjects.
+                                                </p>
+                                                <p>
+
+                                                    It all started with an idea of preparing for the GATE(Graduate Aptitude Test in Engineering). 
+                                                    When we started the preparation for the Test, we have to go through alot of websites but found it very diﬃcult to ﬁnd everything at one place. So our team decided to make this application for web-users.
+                                                </p>
+                                                <p>
+
+                                                    The basic idea behind this application is to gather all the concepts and subjects together and 
+                                                    provide an interactive way for users to prepare for the exam as well as to brush up there knowl-
+                                                    edge. In this a user can test his/her knowledge going through various diﬃculty levels (Beginners-
+                                                    Intermediate-Expert) and learn alot of fundamental things which we usually ignore.
+                                                </p>
+
+                                                            AGGADH provides a platform for people to study subjects in an interactive manner. They can
+
+                                                            read up on the subject matter and then test their knowledge by taking the test. The more you
+
+                                                            score on the test, the harder the level be. This will not only improve and test the knowledge of
+
+                                                            the candidate but will make him compete against himself.
+
+                                                            Keep record of number of attempts you took for a single test. Improve your marks and percentage
+
+                                                            by taking a Re-test.
+
+                                                            Let AGGADH be your mentor and guide towards the inﬁnite knowledge.
 						</p>
 					</div>	
+                                        
+                                        
+                                        <div id="about2">
+						<div class="title"><h3>Motive</h3></div>
+						
+
+                                                <p> AGGADH provides a platform for people to study subjects in an interactive manner. They can
+
+                                                            read up on the subject matter and then test their knowledge by taking the test. The more you
+
+                                                            score on the test, the harder the level be. This will not only improve and test the knowledge of
+
+                                                            the candidate but will make him compete against himself.
+                                                </p>
+
+                                                            
+
+                                                <p> <i>Let AGGADH be your mentor and guide towards the inﬁnite knowledge.</i>
+                                                    
+                                                </p>
+						
+					</div>
 					<!-- end: About Us -->
 
-					<!-- start: History -->
+					<!-- start: History
 					<div id="history">
 						<div class="title"><h3>History</h3></div>
 						<p>
@@ -63,12 +103,14 @@
 							Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
 						</p>
 					</div>	
-					<!-- end: History -->
+					 end: History -->
 
 					<!-- start: Team -->	
 					<div id="team">
 						
 						<div class="title"><h3>Team</h3></div>
+                                                <p>We are a very small and dedicated Team who aim to take this project to new heights. We are working hard every day in our free time to make AGGADH a bit better.</p>
+                                                <p>We also hope this list grows and ambitious people join our Project.</p>
 						
 						<!-- start: Row -->
 						<div class="row">
@@ -76,17 +118,18 @@
 							<!-- start: About Member -->
 							<div class="span4">
 								<div class="avatar view-team">
-									<img src="img/team_member.jpg" alt="team member">
+									<img src="img/dds.png" alt="Damandeep Singh">
 									<div class="mask">
 										<h2>About Me</h2>
 										<p>
-											Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
-										</p>
-										<a href="#" class="info">My Personal Page</a>
+                                                                                    I am Damandeep Singh, student of B.Tech (Computer Science Engg.) at Guru Nanak Dev Engg. College, Ludhiana. I am a experienced Web and Mobile Developer (Android & iOS) with quite a few projects running well.
+                                                                                    <br/><br/>You can contact me on my Personal Website.</p>
+                                                                                
+										<a href="http://singhdd.com" class="info">My Personal Page</a>
 									</div>
 								</div>
 								<div class="clear"></div>
-								<div class="team-name">Lucas Luck <span> / Co-Founder, General Manager</span></div>
+								<div class="team-name">Damandeep Singh <span> / Co-Founder, Lead Developer</span></div>
 								<div class="social_profiles">
 									<ul class="social-bookmarks">
 										<li class="facebook"><a href="">facebook</a></li>
@@ -102,7 +145,7 @@
 							<!-- start: About Member -->
 							<div class="span4">
 								<div class="avatar view-team">
-									<img src="img/team_member.jpg" alt="team member">
+                                                                    <img src="img/scott.png" alt="Gurjot Singh">
 									<div class="mask">
 										<h2>About Me</h2>
 										<p>
@@ -112,7 +155,7 @@
 									</div>
 								</div>
 								<div class="clear"></div>
-								<div class="team-name">Lucas Luck <span> / Co-Founder, Project Manager</span></div>
+								<div class="team-name">Gurjot Singh <span> / Co-Founder, Lead Developer</span></div>
 								<div class="social_profiles">
 									<ul class="social-bookmarks">
 										<li class="facebook"><a href="">facebook</a></li>
@@ -128,7 +171,7 @@
                                                         <!-- start: About Member -->
 							<div class="span4">
 								<div class="avatar view-team">
-									<img src="img/team_member.jpg" alt="team member">
+                                                                    <img src="img/inder.png" alt="Gurwinder Singh">
 									<div class="mask">
 										<h2>About Me</h2>
 										<p>
@@ -138,7 +181,7 @@
 									</div>
 								</div>
 								<div class="clear"></div>
-								<div class="team-name">Lucas Luck <span> / Co-Founder, Project Manager</span></div>
+								<div class="team-name">Gurwinder Singh <span> / Co-Founder, Developer, Content Writer</span></div>
 								<div class="social_profiles">
 									<ul class="social-bookmarks">
 										<li class="facebook"><a href="">facebook</a></li>									
@@ -153,89 +196,7 @@
 						
 						</div>
 						<!-- end: Row -->
-                                                <div class="row">
-						
-							<!-- start: About Member -->
-							<div class="span4">
-								<div class="avatar view-team">
-									<img src="img/team_member.jpg" alt="team member">
-									<div class="mask">
-										<h2>About Me</h2>
-										<p>
-											Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
-										</p>
-										<a href="#" class="info">My Personal Page</a>
-									</div>
-								</div>
-								<div class="clear"></div>
-								<div class="team-name">Lucas Luck <span> / Co-Founder, General Manager</span></div>
-								<div class="social_profiles">
-									<ul class="social-bookmarks">
-										<li class="facebook"><a href="">facebook</a></li>
-										<li class="googleplus"><a href="#">googleplus</a></li>
-										<li class="linkedin"><a href="#">linkedin</a></li>
-										<li class="twitter"><a href="#">twitter</a></li>
-										<li class="github"><a href="#">github</a></li>
-									</ul>
-								</div>	
-							</div>
-							<!-- end: About Member -->
-
-							<!-- start: About Member -->
-							<div class="span4">
-								<div class="avatar view-team">
-									<img src="img/team_member.jpg" alt="team member">
-									<div class="mask">
-										<h2>About Me</h2>
-										<p>
-											Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
-										</p>
-										<a href="#" class="info">My Personal Page</a>
-									</div>
-								</div>
-								<div class="clear"></div>
-								<div class="team-name">Lucas Luck <span> / Co-Founder, Project Manager</span></div>
-								<div class="social_profiles">
-									<ul class="social-bookmarks">
-										<li class="facebook"><a href="">facebook</a></li>
-										<li class="googleplus"><a href="#">googleplus</a></li>
-										<li class="linkedin"><a href="#">linkedin</a></li>
-										<li class="twitter"><a href="#">twitter</a></li>
-										<li class="github"><a href="#">github</a></li>
-									</ul>
-								</div>	
-							</div>
-							<!-- end: About Member -->
-                                                        
-                                                        <!-- start: About Member -->
-							<div class="span4">
-								<div class="avatar view-team">
-									<img src="img/team_member.jpg" alt="team member">
-									<div class="mask">
-										<h2>About Me</h2>
-										<p>
-											Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
-										</p>
-										<a href="#" class="info">My Personal Page</a>
-									</div>
-								</div>
-								<div class="clear"></div>
-								<div class="team-name">Lucas Luck <span> / Co-Founder, Project Manager</span></div>
-								<div class="social_profiles">
-									<ul class="social-bookmarks">
-										<li class="facebook"><a href="">facebook</a></li>									
-										<li class="googleplus"><a href="#">googleplus</a></li>
-										<li class="linkedin"><a href="#">linkedin</a></li>
-										<li class="twitter"><a href="#">twitter</a></li>
-										<li class="github"><a href="#">github</a></li>
-									</ul>
-								</div>	
-							</div>
-							<!-- end: About Member -->
-						
-						</div>
-						<!-- end: Row -->
-
+                                                
 					</div>
 					<!-- end: Team -->
 					
